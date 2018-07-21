f7ac7edd65c503974111666dae8a2888d20225e8
Tim Shenin <shenin.timofei@yandex.ru>
2018-07-21 15:49:53 +0300

    Setup task runner server

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
 
 const postcssPlugins = [
     autoprefixer({browsers: ['last 1 version']})
@@ -12,7 +14,7 @@ const postcssPlugins = [
 gulp.task('html', () => {
     return gulp.src('./src/**/*.pug')
         .pipe(pug())
-        .pipe(gulp.dest('./build/html'))
+        .pipe(gulp.dest('./build'))
 });
 
 gulp.task('sass', () => {
@@ -20,8 +22,17 @@ gulp.task('sass', () => {
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss(postcssPlugins))
-        .pipe(gulp.dest('./build/css'));
+        .pipe(gulp.dest('./build'))
+        .pipe(reload({ stream: true }));
 });
 
+gulp.task('pug-watch', ['html'], () => {
+    return reload();
+});
 
-gulp.task('default', ['html', 'sass']);
+gulp.task('default', ['sass', 'html'], () => {
+    browserSync({ server: './build' });
+
+    gulp.watch('./src/**/*.scss', ['sass']);
+    gulp.watch('./src/**/*.pug', ['pug-watch']);
+});
