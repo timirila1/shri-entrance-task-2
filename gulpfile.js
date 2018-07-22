8a1b2a1c6639f1722bd24d076a9397bf9039db53
Tim Shenin <shenin.timofei@yandex.ru>
2018-07-22 14:44:59 +0300

    Structure of main parts, footer and header desktop

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,48 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
+const concat = require('gulp-concat');
 
 const postcssPlugins = [
-    autoprefixer({browsers: ['last 1 version']})
+    autoprefixer({
+        browsers: ['last 1 version']
+    })
 ];
 
 gulp.task('html', () => {
-    return gulp.src('./src/**/*.pug')
+    gulp.src('./src/**/*.pug')
         .pipe(pug())
         .pipe(gulp.dest('./build'))
 });
 
 gulp.task('sass', () => {
-    return gulp.src('./src/**/*.scss')
+    gulp.src([
+            './src/**/*.scss',
+            './src/normalize.css'
+        ])
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss(postcssPlugins))
+        .pipe(concat('index.css'))
         .pipe(gulp.dest('./build'))
-        .pipe(reload({ stream: true }));
+        .pipe(reload({
+            stream: true
+        }))
 });
 
 gulp.task('pug-watch', ['html'], () => {
-    return reload();
+    reload()
 });
 
-gulp.task('default', ['sass', 'html'], () => {
-    browserSync({ server: './build' });
+gulp.task("images", () => {
+    gulp.src('./src/images/*.png')
+        .pipe(gulp.dest('build/images'))
+});
+
+gulp.task('default', ['sass', 'html', 'images'], () => {
+    browserSync({
+        server: './build'
+    });
 
     gulp.watch('./src/**/*.scss', ['sass']);
     gulp.watch('./src/**/*.pug', ['pug-watch']);
